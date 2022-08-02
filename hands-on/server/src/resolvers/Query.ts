86dd816d82fe400788c18c8fc0d64bbcfcf5238b
bruhtus <hibruhtus@gmail.com>
2022-08-03 04:33:25 +0700

    feat(Query.ts): return data from database for suggestions

--- hands-on/server/src/resolvers/Query.ts
+++ hands-on/server/src/resolvers/Query.ts
@@ -13,23 +13,8 @@ const queryTwitterResolver: QueryResolvers<TwitterResolverContext> = {
       updatedAt: '',
     };
   },
-  suggestions: (_, __, { db: _db }) => {
-    return [
-      {
-        name: 'TypeScript Project',
-        handle: 'TypeScript',
-        avatarUrl: 'http://localhost:3000/static/ts-logo.png',
-        reason: 'Because you follow @MichaelLNorth',
-        id: '1',
-      },
-      {
-        name: 'jQuery',
-        handle: 'jquery',
-        avatarUrl: 'http://localhost:3000/static/jquery-logo.jpeg',
-        reason: 'Because you follow @FrontendMasters',
-        id: '2',
-      },
-    ];
+  suggestions: (_, __, { db }) => {
+    return db.getAllSuggestions();
   },
 };
 
