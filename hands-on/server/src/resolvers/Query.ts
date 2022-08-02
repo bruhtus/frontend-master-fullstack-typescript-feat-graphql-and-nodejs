b01be5677165b54ca2cd15b777f9e24efd9adf1b
bruhtus <hibruhtus@gmail.com>
2022-08-03 05:42:35 +0700

    feat(Query.ts): return data from database for current user

--- hands-on/server/src/resolvers/Query.ts
+++ hands-on/server/src/resolvers/Query.ts
@@ -2,17 +2,19 @@ import { TwitterResolverContext } from '../resolvers';
 import { QueryResolvers } from '../resolvers-types.generated';
 
 const queryTwitterResolver: QueryResolvers<TwitterResolverContext> = {
-  currentUser: () => {
-    return {
-      id: '123',
-      name: 'John Doe',
-      handle: 'johndoe',
-      coverUrl: '',
-      avatarUrl: '',
-      createdAt: '',
-      updatedAt: '',
-    };
+  currentUser: (_, __, { db }) => {
+    const [firstUser] = db.getAllUsers();
+    // Note:
+    // we need to add the guard so that there is no possibility that first user
+    // is null, otherwise typescript and graphql server will yell at us.
+    if (!firstUser) {
+      throw new Error(
+        'currentUser was requested, but there are no users in the database'
+      );
+    }
+    return firstUser;
   },
+
   suggestions: (_, __, { db }) => {
     return db.getAllSuggestions();
   },
