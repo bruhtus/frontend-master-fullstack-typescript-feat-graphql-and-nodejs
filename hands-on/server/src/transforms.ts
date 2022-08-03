5752f69ec99ac6c65745434cc4cd213bb1cac1be
bruhtus <hibruhtus@gmail.com>
2022-08-03 09:58:30 +0700

    feat(favorites): fetch likes/favorites data from database
    note:
    this is only fetch the data, we still can add or delete likes/favorites.

--- hands-on/server/src/transforms.ts
+++ hands-on/server/src/transforms.ts
@@ -1,5 +1,6 @@
-import { Tweet } from './resolvers-types.generated';
+import { Favorite, Tweet } from './resolvers-types.generated';
 import { DbTweet } from './db';
+import { DbFavorite } from './db';
 
 // Note:
 // author is related record that is a field of typed User and that is going to
@@ -8,6 +9,16 @@ export const tweetTransform = (t: DbTweet): Omit<Tweet, 'author'> => {
   return {
     id: t.id,
     body: t.message,
+    createdAt: t.createdAt,
+    updatedAt: t.updatedAt,
+  };
+};
+
+export const favoriteTransform = (
+  t: DbFavorite
+): Omit<Favorite, 'user' | 'tweet'> => {
+  return {
+    id: t.id,
     createdAt: t.createdAt,
     updatedAt: t.updatedAt,
   };
